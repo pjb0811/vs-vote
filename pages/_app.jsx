6f20b0f464164472b0469142cea8a653cd49c94f
jbpark <jbpark@wemakeprice.com>
2018-11-06 17:17:17 +0900

    update next.js setting

---
+++ pages/_app.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+
+class CustomApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    return {
+      pageProps: {
+        ...(Component.getInitialProps
+          ? await Component.getInitialProps(ctx)
+          : null)
+      }
+    };
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { Component, pageProps, router } = this.props;
+
+    return (
+      <Container>
+        <Component {...pageProps} router={{ ...router }} />
+      </Container>
+    );
+  }
+}
+
+export default CustomApp;
---
+++ pages/_document.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+
+export default class CustomDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
